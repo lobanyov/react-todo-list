bb2d68964b4225854b609db9a35b0537b27c85a5
lobanyov <stevenson@tut.by>
2020-02-27 11:26:37 +0300

    Apply bootstrap

--- todo-list/src/components/todo-list-item/todo-list-item.js
+++ todo-list/src/components/todo-list-item/todo-list-item.js
@@ -11,7 +11,27 @@ const TodoListItem = ({ description, important }) => {
     fontWeight: important ? 'bold' : 'normal', 
   }
 
-  return <span className="todo-list-item" style={style}>{ description }</span>;
+  return (
+    <span className="todo-list-item">
+      <span 
+        className="todo-list-item-label"
+        style={style}>
+          { description }
+      </span>
+
+      <button 
+        type="button"
+        className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+      </button>
+
+      <button 
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 }
 
 export default TodoListItem;
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.scss';
+
 import AppHeader from './components/app-header/app-header';
 import SearchPanel from './components/search-panel/search-panel';
 import TodoList from './components/todo-list/todo-list';
+import ItemStatusFilter from './components/item-status-filter/item-status-filter';
 
 const App = () => {
 
@@ -11,14 +14,18 @@ const App = () => {
     { description: '11-00-AM Meeting', important: true, key: '_QFY_23' },
     { description: '13-00-PM Lunch', important: false, key: '_TNY_41' },
     { description: '15-00-PM Code review', important: true, key: '_MVB_19' },
-    { description: '17-00-PM Learning React + Redux', important: false, key: '_KZX_17' }
+    { description: '17-00-PM React + Redux', important: false, key: '_KZX_17' }
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData} />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3}/>
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} /> 
     </div>
   );
 }
