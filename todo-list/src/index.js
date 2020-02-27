fcabf10c74601293e437bc70d4ab4a3599374829
lobanyov <stevenson@tut.by>
2020-02-27 11:56:40 +0300

    refactor: add index.js for each dist

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.scss';
 
-import AppHeader from './components/app-header/app-header';
-import SearchPanel from './components/search-panel/search-panel';
-import TodoList from './components/todo-list/todo-list';
-import ItemStatusFilter from './components/item-status-filter/item-status-filter';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
 const App = () => {
 
